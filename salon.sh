6714cecd164d2a7b8a689b4c77db46c7d140d3d0
Esteban Flamini <38509691+estebanflamini@users.noreply.github.com>
2026-02-07 20:14:02 +0200

    Completed Salon Appointment assignment

---
+++ salon.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Have psql output CSV data with only tuples.
+PSQL='psql --username=freecodecamp --dbname=salon -t --no-align -c'
+
+show_services() {
+  $PSQL "SELECT service_id, name FROM services" | while IFS='|' read ID NAME ; do
+    echo "$ID) $NAME"
+  done
+}
+
+echo -e "\n~~~~~ MY SALON ~~~~~\n"
+echo -e "Welcome to My Salon, how can I help you?\n"
+
+while true ; do
+
+  show_services
+
+  # Get the desired service
+  read SERVICE_ID_SELECTED
+
+  # Check if it is valid. and in passing, get the service's name
+  SERVICE_NAME="$($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED")"
+  if [[ -z $SERVICE_NAME ]] ; then
+
+    # No valid ID selected, tell the customer and continue the loop
+    echo -e "\nI could not find that service. What would you like today?"
+
+  else
+
+    # Get the customer's phone
+    echo -e "\nWhat's your phone number?"
+    read CUSTOMER_PHONE
+
+    # Get the customer's name from the database
+    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
+
+    # If not found...
+    if [[ -z $CUSTOMER_NAME ]] ; then
+      # Ask the new customer's name
+      echo -e "\nI don't have a record for that phone number, what's your name?"
+      read CUSTOMER_NAME
+      # Insert the new customer
+      $PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')" > /dev/null
+    fi
+
+    # Get the customer's ID
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+
+    # Get the service time.
+    echo -e "\nWhat time would you like your $SERVICE_NAME, ${CUSTOMER_NAME}?"
+    read SERVICE_TIME
+
+    # Register the appointment
+
+    $PSQL "INSERT INTO appointments(customer_id,service_id,time) VALUES($CUSTOMER_ID,$SERVICE_ID_SELECTED,'$SERVICE_TIME')" > /dev/null
+
+    echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME.\n"
+
+    break
+
+  fi
+done
+
